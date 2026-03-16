981c583be2ee171f5821df4e70ec1ade5f16b157
JunLee8108 <lejhn1@gmail.com>
2026-03-16 12:54:07 -0500

    Initial Commit

---
+++ Bapsang/BapsangApp.swift
@@ -0,0 +1,17 @@
+//
+//  BapsangApp.swift
+//  Bapsang
+//
+//  Created by Jun Lee on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct BapsangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BapsangTests/BapsangTests.swift
@@ -0,0 +1,17 @@
+//
+//  BapsangTests.swift
+//  BapsangTests
+//
+//  Created by Jun Lee on 3/16/26.
+//
+
+import Testing
+@testable import Bapsang
+
+struct BapsangTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
